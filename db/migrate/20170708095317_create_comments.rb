76e12ffbd7abfbe5e8f336f1131a04947c7eeb9e
chanweiyan <chanweiyan007@gmail.com>
2017-07-08 18:33:05 +0800

    implement comments#create function

--- db/migrate/20170708095317_create_comments.rb
+++ db/migrate/20170708095317_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
-      t.string :comment
+      t.string :commenter
       t.text :body
       t.references :article, foreign_key: true
 
