e09723e8d2db5d49ae2a82cadcb50228e5034ea3
chanweiyan <chanweiyan007@gmail.com>
2017-07-08 17:31:41 +0800

    implement articles#update function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
@@ -19,8 +23,18 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
-    end 
+    end
   end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end 
 
   private
   def article_params
